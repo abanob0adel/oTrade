eeea1ffd329ff3315fb1c839dd16d134649a0d8e
YAS <yas@MacBook-Air.local>
2026-02-23 21:42:55 +0200

    update analysis

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const corsOptions = {
     'https://otrade-admin.vercel.app',
     "https://www.otrade.ae",
     "otrade.ae",
-    "portal.otrade.ae",
+    "http://portal.otrade.ae/",
     'https://o-trade-front.vercel.app'
   ],
   credentials: true,
